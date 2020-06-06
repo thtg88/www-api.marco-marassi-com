1e6e528f4a931f0e3b3573bf38d2e56d9e0a52fc
Marco Marassi <thtg88@users.noreply.github.com>
2020-06-07 00:16:08 +0100

    Added AWS SQS configuration variables

--- config/queue.php
+++ config/queue.php
@@ -51,12 +51,12 @@ return [
 
         'sqs' => [
             'driver' => 'sqs',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'key' => env('AWS_SQS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SQS_SECRET_ACCESS_KEY'),
             'prefix' => env('SQS_PREFIX', 'https://sqs.us-east-1.amazonaws.com/your-account-id'),
             'queue' => env('SQS_QUEUE', 'your-queue-name'),
             'suffix' => env('SQS_SUFFIX'),
-            'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+            'region' => env('AWS_SQS_DEFAULT_REGION', 'us-east-1'),
         ],
 
         'redis' => [
